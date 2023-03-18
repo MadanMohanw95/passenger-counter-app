ac2fb4452f03d2424ae51b339e146aab8035db32
Vanga Madan Mohan Reddy <madhanmohanw95@gmail.com>
2023-03-18 21:24:07 +0000

    passenger feature

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-document.getElementById("count-el").innerText = 5
+let counterEl = document.getElementById('count-el');
+let saveEl = document.getElementById('save-el');
+let passengerCount = 0;
+
+function increment() {
+    passengerCount += 1;
+    counterEl.textContent = passengerCount;
+}
+
+let previousEntries = '';
+function save() {
+    const previousEntries = passengerCount + ' - ';
+    if (passengerCount) {
+        saveEl.textContent += previousEntries;
+    }
+    console.log(previousEntries)
+}
+
+function reset() {
+    passengerCount = 0;
+    counterEl.textContent = passengerCount;
+}
